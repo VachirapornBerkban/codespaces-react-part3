f048055590067e9f7ca78b78b76ed8efbbed6f92
VachirapornBerkban <Vachiraphon.b@ku.th>
2024-10-17 08:50:56 +0000

    Implement add/delete/update

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -7,11 +7,18 @@ function Item(props){
         id: {props.id} <br/>
         name: {props.name}<br/>
         price: {props.price}<br/>
+        <button onClick={()=>props.del_callback(props.id)}>delete</button>
+        <button onClick={()=>props.del_callback(props.id)}>delete</button>
     </div>);
 }
 export default function Shop(){
-        const [products,setProducts]=useState([]);
-        const URL="https://solid-carnival-v6gjpx755grjhx4p6-5000.app.github.dev";
+    let id;
+    const name_ref=useRef(null);
+    const price_ref=useRef(null);
+    const img_ref=useRef(null);
+    const [products,setProducts]=useState([]);
+    const URL="https://solid-carnival-v6gjpx755grjhx4p6-5001.app.github.dev";
+
         useEffect(()=>{
             axios.get(URL+'/api/products')
             .then(response=>{
@@ -38,7 +45,63 @@ export default function Shop(){
         for(let i=0;i<cart.length;i++){
             totalprice+=cart[i].price;
         }
+        function addProduct(){
+            const data={
+            name:name_ref.current.value,
+            price:price_ref.current.value,
+            img:img_ref.current.value
+            };
+            axios.post(URL+'/api/products',data)
+            .then(response=>{
+                setProducts(response.data);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+        
+        function delProduct(id){
+            axios.delete(URL+'/api/products'+id)
+            .then(response=>{
+                if (respond.data.status=="ok") alert("Delete product sucsessfully!");
+                setProducts(response.data.products);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+
+        function updateProductForm(item){
+            id=item.id;
+            name_ref.current.value=item.name;
+            price_ref.current.value=item.price;
+            img_ref.current.value=item.img;
+        }
+
+        function updateProduct(item){
+            const data={
+                name:item.name,
+                price:item.price,
+                img:item.img
+                };
+            axios.put(URL+'/api/updateproducts'+id)
+            .then(response=>{
+                if (respond.data.status=="ok") alert("UPDATE product sucsessfully!");
+                setProducts(response.data.products);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+        
+
+
         return (<>
+        name : <input type="text" ref={(name_ref)}/>
+        price : <input type="text" ref={(price_ref)}/>
+        img : <input type="text" ref={(img_ref)}/>
+        <button onClick={addProduct}>add</button>
+
         <div className='grid-container'>{productList}</div>
         <h1>Cart</h1>
         <button onClick={()=>setCart([])}>Clear all</button>
